e5f24e52a498d224a7a0f6863d0a0441e6d36d36
Darfi Omar <darfi.omar@gmail.com>
2021-01-25 01:37:13 +0100

    Project commit

---
+++ src/main/java/fr/ubo/tpspring/entities/Greeting.java
@@ -0,0 +1,16 @@
+package fr.ubo.tpspring.entities;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Greeting {
+
+    private long id;
+    private String content;
+
+
+}
